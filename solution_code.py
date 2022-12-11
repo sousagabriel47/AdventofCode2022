6ca72774ec26ab9e53b9e941ecfd61cd0e19ba0b
Gabriel <r323709@opmetro.local>
2022-12-11 08:38:00 -0300

    day2

--- solution_code.py
+++ solution_code.py
@@ -4,8 +4,9 @@ class Solutions(object):
     """One funciton per day."""
     def __init__(self, data):
         """Initialize."""
-        self.dict_function = {}
-        self.dict_function['day_1'] = self.day_1(data)
+        self.dict_function = {
+            'day_1': self.day_1(data),
+            'day_2': self.day_2(data)}
 
 
     def day_1(init, data):
@@ -25,12 +26,35 @@ class Solutions(object):
             soma = 0
         print(f'Max Calories: {max}')
         print(f'Top3 Calories: {sum(top3)}')
-         
 
+    def day_2(init, data):
+        """Solution of day2."""
+        score = {
+            'A X': 3,
+            'A Y': 6,
+            'A Z': 0,
+            'B X': 0,
+            'B Y': 3,
+            'B Z': 6,
+            'C X': 6,
+            'C Y': 0,
+            'C Z': 3}
+        bonus = {
+            'X': 1,
+            'Y': 2,
+            'Z': 3}
+        sum_score = 0
+        turns = data.split()
+        for turn in turns:
+            sum_score += score[turn] + bonus[turn[2]]
+
+        print(f'Total Score: {sum_score}')
 
 if __name__ == "__main__":
-    nday = input('Day :')
+    nday = int(input('Day :'))
     with open(f'day{nday}','r') as file:
         data = file.read()
+
+    print(f'####### DAY {nday} #######')
     s = Solutions(data)
     s.dict_function[f'day_{nday}']
