9a5ff9f6fa469beed3c6e2106a73cdaa360cc206
Gabriel <r323709@opmetro.local>
2022-12-11 07:18:05 -0300

    commit inicial - day1

---
+++ code.py
@@ -0,0 +1,11 @@
+"""Solution of Advent of Code 2022."""
+
+
+class Solutions(object):
+    """One funciton per day."""
+    def __init__(self):
+        """Initialize."""
+
+    def day_1(init, data):
+        """Solution of day1."""
+        
